642b7dedac06df47dbff7893ae406e222fc6ff4d
Peter Burns <peterburnshicks@gmail.com>
2018-01-25 19:19:24 -0500

    added post and get commands for table and waitlist

--- server.js
+++ server.js
@@ -23,11 +23,31 @@ app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/tables/view", function (req, res) {
+    res.json(tables);
+});
+
+app.get("/waitlist/view", function(req, res) {
+    res.json(waitList);
+});
+
+app.post("/tables/add", function (req, res) {
+    var newTable = req.body;
+    if (tables.length < 5) {
+        tables.push(newTable);
+    }
+    else {
+        waitList.push(newTable);
+    }
+    res.json(newTable);
+});
+
 app.post("/tables/clear", function (req, res) {
     waitList = [];
     tables = [];
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.json({});
 });
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
